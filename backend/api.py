cd6c31e428a03bbd795dfd8e6fb8cc49c2b64629
mianivo <andrey.m@gmail.com>
2025-04-15 19:23:55 +0500

    add russian translate. Update requirements.txt file

--- backend/api.py
+++ backend/api.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, UploadFile, HTTPException
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
 import torch
 import io
-from huggingface_hub import login
 import random
-import os
-login(token=os.getenv("HUGGINGFACE_TOKEN"))  # для доступа к моделям Gemma необходима авторизация
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import MarianMTModel, MarianTokenizer
 
+import os
+from dotenv import load_dotenv
+from huggingface_hub import login
+
+load_dotenv()
+token = os.getenv("HUGGINGFACE_TOKEN")  # для доступа к моделям Gemma необходима авторизация
+login(token=token)
 
 english_rule_topics = [
     "Pronouns",
@@ -62,6 +66,7 @@ blip_processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning
 blip_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 print("BLIP model downloaded")
 
+
 def get_image_keywords(image: Image) -> dict:
     """Анализирует предложение и возвращает его текстовое описание"""
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -79,16 +84,26 @@ llm_tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
 llm = AutoModelForCausalLM.from_pretrained(
     "google/gemma-2b-it",
     device_map="auto",
-    torch_dtype=torch.bfloat16
 )
 
 print("LLM downloaded")
 
+print("Starting loading translate model")
+
+translate_model_name = "Helsinki-NLP/opus-mt-en-ru"
+translate_tokenizer = MarianTokenizer.from_pretrained(translate_model_name)
+translate_model = MarianMTModel.from_pretrained(translate_model_name)
+
+print("translate model downloaded")
 
 input_text = "Generate 1 sentence in English using grammar on the topic '{}' " \
              "using words from the sentence '{}'. The answer must contains 1 sentence ONLY. " \
              "The sentence must include words from the given sentence and must use the rules on the given topic. " \
              "The answer must NOT contain any other."
+
+input_text_for_translate = "Translate this text into Russian language. {} There" \
+                           " should be nothing superfluous in the answer. " \
+                           "The answer should contain ONLY THE TRANSLATION."
 
 
 def get_formated_prompt(image_description: str) -> str:
@@ -96,20 +111,38 @@ def get_formated_prompt(image_description: str) -> str:
     return input_text.format(rule_name, image_description)
 
 
+def run_llm(input_text: str) -> str:
+    last_prompt_sentence = input_text.split(".")[-2]
+    input_ids = llm_tokenizer(input_text, return_tensors="pt").to("cuda")
+    outputs = llm.generate(**input_ids, max_length=1000)
+    llm_output_text = \
+        llm_tokenizer.decode(outputs[0], skip_special_tokens=True).split(last_prompt_sentence)[
+            -1].split("\n\n")[-1]
+    return llm_output_text
+
+
+def generate_english_sentence(image_description: str) -> str:
+    input_text = get_formated_prompt(image_description)
+    llm_output_text = run_llm(input_text)
+    return llm_output_text
+
+
+def generate_translation(text: str) -> str:
+    inputs = translate_tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+    translated = translate_model.generate(**inputs)
+    return translate_tokenizer.decode(translated[0], skip_special_tokens=True)
+
+
 def analyze_image(image: Image = None) -> dict:
     """Генерирует предложение на основе изображения"""
     if image:
         image_description = get_image_keywords(image)["keywords"]
-
-        input_text = get_formated_prompt(image_description)
-        input_ids = llm_tokenizer(input_text, return_tensors="pt").to("cuda")
-        outputs = llm.generate(**input_ids, max_length=1000)
-
-        llm_output_text = \
-        llm_tokenizer.decode(outputs[0], skip_special_tokens=True).split("The answer must NOT contain any other.")[-1].split("\n\n")[-1]
+        english_sentence = generate_english_sentence(image_description)
+        tranleted_sentence = generate_translation(english_sentence)
 
         return {
-            "sentence": llm_output_text
+            "sentence": english_sentence,
+            "translate": tranleted_sentence
         }
     else:
         raise ValueError('No image to analyze')
